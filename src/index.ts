e20cc6da8e4e23bbb822965fe28eb4e9ebf1b452
nyan7natsu <166627121+nyan7natsu@users.noreply.github.com>
2025-09-24 14:22:11 +0900

    init

---
+++ src/index.ts
@@ -0,0 +1,130 @@
+const CIT_PORTAL_URL = "https://portal.it-chiba.ac.jp";
+const UPRX_WEBAPI = "/uprx/webapi/up";
+
+import { LoginResponse, ClassScheduleResponse } from "./types";
+
+/**
+ * CITClass Class
+ */
+export class CITClass {
+  private fetch: (
+    input: RequestInfo | URL,
+    init?: RequestInit
+  ) => Promise<Response>;
+
+  private LOGIN_URL = CIT_PORTAL_URL + UPRX_WEBAPI + "/pk/Pky001Resource/login";
+  private CLASS_URL =
+    CIT_PORTAL_URL + UPRX_WEBAPI + "/ap/Apa004Resource/getJugyoKeijiMenuInfo";
+
+  /**
+   * CITClass Class Contructor
+   * @param fetchImpl NodeJSデフォルトのfetchではなく，独自のリクエストメソッドを使いたい場合に指定する
+   */
+  constructor(
+    fetchImpl: (
+      input: RequestInfo | URL,
+      init?: RequestInit
+    ) => Promise<Response> = fetch
+  ) {
+    this.fetch = fetchImpl;
+  }
+
+  /**
+   * ログインを行う
+   * @param userId ユーザーID (アルファベット1文字 + 学籍番号)
+   * @param password パスワード
+   * @returns ログイン結果
+   */
+  public async login(userId: string, password: string): Promise<LoginResponse> {
+    try {
+      const resp = await this.fetch(this.LOGIN_URL, {
+        method: "POST",
+        body: JSON.stringify({
+          data: {
+            loginUserId: userId,
+            plainLoginPassword: password,
+          },
+        }),
+      });
+      const body = decodeURIComponent(await resp.text());
+      const data: LoginResponse = JSON.parse(body);
+      return data;
+    } catch (e) {
+      console.error("Login error:", e);
+      return {
+        statusDto: {
+          success: false,
+          messageList: ["CITポータルとの通信に失敗しました"],
+        },
+        data: null,
+      };
+    }
+  }
+
+  /**
+   * 授業時間割の取得を行う
+   *
+   * パスワードまたは暗号化されたパスワードのどちらか一方を指定してください。
+   * 両方の指定及び両方の未指定はエラーとなります。
+   *
+   * @param userId ユーザーID (アルファベット1文字 + 学籍番号)
+   * @param password パスワード
+   * @param encryptedPassword 暗号化されたパスワード (ログイン時に取得可能)
+   * @returns 授業時間割取得結果
+   */
+  public async getClassSchedule(
+    userId: string,
+    password: string
+  ): Promise<ClassScheduleResponse>;
+  public async getClassSchedule(
+    userId: string,
+    encryptedPassword: string
+  ): Promise<ClassScheduleResponse>;
+
+  public async getClassSchedule(
+    userId: string,
+    password: string = "",
+    encryptedPassword: string = ""
+  ): Promise<ClassScheduleResponse> {
+    if (
+      (password === "" && encryptedPassword === "") ||
+      (password !== "" && encryptedPassword !== "")
+    ) {
+      throw new Error(
+        "パスワードまたは暗号化されたパスワードのどちらか一方を指定してください"
+      );
+    }
+    try {
+      const resp = await this.fetch(this.CLASS_URL, {
+        method: "POST",
+        body: JSON.stringify({
+          productCd: "ap",
+          langCd: "",
+          loginUserId: userId,
+          data: {
+            autoLoginAuthCd: "",
+            kaikoNendo: 0,
+            gakkiNo: 0,
+            deviceId: "",
+          },
+          plainLoginPassword: password,
+          subProductCd: "apa",
+          encryptedLoginPassword: encryptedPassword,
+          deviceId: "",
+        }),
+      });
+      const body = decodeURIComponent(await resp.text());
+      const data: ClassScheduleResponse = JSON.parse(body);
+      return data;
+    } catch (e) {
+      console.error("Get class schedule error:", e);
+      return {
+        statusDto: {
+          success: false,
+          messageList: ["CITポータルとの通信に失敗しました"],
+        },
+        data: null,
+      };
+    }
+  }
+}
---
+++ src/types.ts
@@ -0,0 +1,112 @@
+// NOTE: `any | null` (実際には`any`になる) は，APIの仕様がはっきりしないため暫定的にこうしている
+
+export type LoginResponse =
+  | {
+      statusDto: {
+        success: true;
+        messageList: string[];
+      };
+      data: {
+        userId: string;
+        encryptedPassword: string;
+        userName: string;
+        userShkbtKbn: string;
+        shokuinUserKbn: any | null;
+        jinjiCd: any | null;
+        kanriNo: number;
+        gaksekiCd: string;
+        name: string;
+        nameKana: string;
+        nameEng: string;
+        nameDisp: string;
+        validKikanStartDatetime: any | null;
+        validKikanEndDatetime: any | null;
+        menuPtnCd: any | null;
+        langCd: string;
+      };
+    }
+  | {
+      statusDto: {
+        success: false;
+        messageList: string[];
+      };
+      data: null;
+    };
+
+export type ClassData = {
+  /** 授業年度 */
+  nendo: number;
+
+  /** 授業コード */
+  jugyoCd: string;
+
+  /** 開講年度 */
+  kaikoNendo: number;
+
+  /** 開講学期 */
+  gakkiNo: number;
+
+  /**
+   * 授業区分
+   *
+   * `"1"`: 通常授業
+   *
+   * `"6"`: 集中授業
+   */
+  jugyoKbn: string;
+
+  /** 授業名 */
+  jugyoName: string;
+
+  /** 授業曜日 */
+  kaikoYobi: number;
+
+  /** 授業時限 */
+  jigenNo: number;
+
+  /**
+   * 授業開始時間
+   *
+   * 集中講義の場合は `null` になる
+   */
+  jugyoStartTime: string | null;
+
+  /**
+   * 授業終了時間
+   *
+   * 集中講義の場合は `null` になる
+   */
+  jugyoEndTime: string | null;
+
+  /** 指導教員名 */
+  kyoinName: string;
+
+  /** 教室名 */
+  kyoustName: string | null;
+
+  keijiMidokCnt: number;
+};
+
+export type ClassScheduleResponse =
+  | {
+      statusDto: {
+        success: true;
+        messageList: string[];
+      };
+      data: {
+        nendo: number;
+        gakkiNo: number;
+        gakkiName: string;
+        jgkmDtoList: ClassData[];
+        keijiCnt: number;
+        funcIdList: string[];
+        maxGakkiNo: number;
+      };
+    }
+  | {
+      statusDto: {
+        success: false;
+        messageList: string[];
+      };
+      data: null;
+    };
